ce6bc42022b791d386f1e2b2b0a5c5fdd1bc39a9
asafamir123 <asaf.amir@gmail.com>
2025-03-24 19:30:20 +0200

    add

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-#a
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!111111111111111111111111111'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port) 
